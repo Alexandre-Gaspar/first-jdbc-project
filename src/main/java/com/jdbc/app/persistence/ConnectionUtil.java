5d5caf365307d8ba4ece13a2ab2ba4cf6ddbe353
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-10-02 12:12:21 +0100

    chore: add connection configuration

---
+++ src/main/java/com/jdbc/app/persistence/ConnectionUtil.java
@@ -0,0 +1,22 @@
+package com.jdbc.app.persistence;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ConnectionUtil {
+
+    private static final String datasourceUrl = "jdbc:postgresql://localhost/sample-jdbc";
+    private static final String datasourceUser = "postgres";
+    private static final String datasourcePassword = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(datasourceUrl, datasourceUser, datasourcePassword);
+    }
+
+}
