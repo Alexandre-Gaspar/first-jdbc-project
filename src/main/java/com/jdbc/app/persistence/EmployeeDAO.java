f5f0384193586fe01a4ef004e9bfbe868c6590bf
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-10-02 12:15:57 +0100

    feat(Persistence): add Employee DAO for crud operations

---
+++ src/main/java/com/jdbc/app/persistence/EmployeeDAO.java
@@ -0,0 +1,29 @@
+package com.jdbc.app.persistence;
+
+import com.jdbc.app.persistence.entity.EmployeeEntity;
+
+import java.util.List;
+
+public class EmployeeDAO {
+
+    public void insert(final EmployeeEntity entity) {
+
+    }
+
+    public void update(final EmployeeEntity entity) {
+
+    }
+
+    public void delete(final Long id) {
+
+    }
+
+    public List<EmployeeEntity> findAll() {
+        return null;
+    }
+
+    public EmployeeEntity findById(final long id) {
+        return null;
+    }
+
+}
