c6f1f50f59564a8ae431d5cec37fc79e7f726f0f
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-10-02 12:06:48 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "com.jdbc.app"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("org.flywaydb:flyway-core:10.18.2")
+
+    // compile dependencies
+    compileOnly("org.projectlombok:lombok:1.18.34")
+
+    // runtime dependencies
+    //runtimeOnly("org.flywaydb:flyway-database-postgresql:10.18.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
